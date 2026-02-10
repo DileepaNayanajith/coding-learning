5e91279159e7e69d961b487b639bd0eb0527e818
DileepaNayanajith <dileepanayanajith380@gmail.com>
2026-02-10 16:48:48 +0530

    updated to Find Sum & Average

--- Data structures/Self leraning/arrays/arrays/array basics.cpp
+++ Data structures/Self leraning/arrays/arrays/array basics.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
 	int A[6];
+	int sum = 0;
 
 	cout << "Enter 6 numbers:" << endl;
 
@@ -16,7 +17,14 @@ int main()
 
 	cout << "\nYou entered:\n";
 
-	for (int i = 0; i < 5; i++) { cout << A[i] << " "; }
+	for (int i = 0; i < 5; i++) 
+	{ 
+		cout << A[i] << " "; 
+		sum = sum + A[i];
+	}
+	cout<<endl;
+	cout << sum << endl;
+	cout << "Average " << sum / 6.0 << endl;
 
 	return 0;
 
