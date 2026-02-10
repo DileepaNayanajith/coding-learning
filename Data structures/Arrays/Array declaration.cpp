63249113e4e6ebcd82f47f8fffa47a944ddc3ad3
DileepaNayanajith <dileepanayanajith380@gmail.com>
2026-02-10 10:44:44 +0530

    Array declaration

---
+++ Data structures/Arrays/Array declaration.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A[5] = { 10,20,40,30,50 };
+	
+	cout << "Elements of the array:"<<endln;
+	 
+	for (int i = 0; i < 5; i++)
+	{
+		cout << arr[i] << "";
+	}
+
+	return;
+}
