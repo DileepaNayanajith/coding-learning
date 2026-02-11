f4eee43548c357d824d882b3640e814690b572bd
DileepaNayanajith <dileepanayanajith380@gmail.com>
2026-02-11 19:31:15 +0530

    if else statement

--- Data structures/Self leraning/Workshop Day 1/Workshop Day 1/Source.cpp
+++ Data structures/Self leraning/Workshop Day 1/Workshop Day 1/Source.cpp
@@ -4,13 +4,28 @@ using namespace std;
 
 int main ()
 {
-	int A,B;
-	cout <<  "ADD NUM A:";
-	cin >> A;
-	cout << "ADD NUM B:";
-	cin >> B;
-
-	cout << "SUM OF A and B = " << A+B <<endl;
+	int X;
+	string pass;
+	cout <<  "What is your marks:";
+	cin >> X;
+	
+	if (X > 75)
+	{
+		pass = "A";
+	}
+	else if (X > 65)
+	{
+		pass = "B";
+	}
+	else if (X > 55)
+	{
+		pass = "C";
+	}
+	else 
+	{
+		pass = "FAIL";
+	}
+	cout << "Your result is :" << pass << endl;
 	
 	return 0;
 }
