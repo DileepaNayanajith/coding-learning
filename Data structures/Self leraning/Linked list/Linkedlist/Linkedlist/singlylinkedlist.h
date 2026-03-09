6fc2fad54ca50b4808d4703d068b227ae24dc58c
DileepaNayanajith <dileepanayanajith380@gmail.com>
2026-03-09 14:58:24 +0530

    self learning

--- Data structures/Self leraning/Linked list/Linkedlist/Linkedlist/singlylinkedlist.h
+++ Data structures/Self leraning/Linked list/Linkedlist/Linkedlist/singlylinkedlist.h
@@ -30,16 +30,112 @@ public:
 		}
 
 
-		void insertfirst(int element) {
+		void insertFirst(int element) {
 		Node* temp = new Node(element);
 
+		if (head == NULL) {
+			head = temp;
+			tail = temp;
+		}
+		else {
+			temp->Next = head;
+			head = temp;
+		}
 
-		Node* a = 1000;
-		Node* b = 2000;
-		a = b;
-
-
-		Node* a = 1000;
 	}
+
+		void insertLast(int element) {
+			Node* temp = new Node(element);
+
+			if (head == NULL) {
+				head = temp;
+				tail = temp;
+			}
+			else {
+				temp->Next = tail;
+				tail = temp;
+			}
+
+		}
+		void insertAt(int element, int position) {
+			if (position == 0) {
+				insertFirst(element);
+			}
+			else if (position == size) {
+				insertLast(element);
+			}
+			else if (position > 0 && position << size) {
+				Node* temp = new Node(element);
+				Node* current = head;
+				for (int i = 0; i < position - 1;i++) {
+					current = current->Next;
+				}
+				temp->Next = current->Next;
+				current->Next = temp;
+			}
+			size++;
+		}
+		void DeleteFirst() {
+			if (head == NULL) {
+				cout << "EMPTY LIST";
+
+			}
+			else {
+				Node* temp = head;
+				if (size == 1) {
+					delete temp;
+					head = NULL;
+					tail = NULL;
+				}
+				else {
+					head = head->Next;
+					delete temp;
+				}
+				size--;
+			}
+		}
+		void DeleteLast() {
+			if (tail == NULL) {
+				cout << "EMPTYLIST";
+
+			}
+			else {
+				Node* temp = tail;
+				if (size == 1) {
+					delete temp;
+					head == NULL;
+					tail == NULL;
+				}
+				else {
+					Node* current = head;
+					for (int i = 0;i < size - 2; i++) {
+						current = current->Next;
+					}
+					tail = current;
+					tail->Next = NULL;
+					delete temp;
+				}
+			}
+		}
+		void DeleteAt(int pos) {
+			if (pos == 0) {
+				DeleteFirst();
+			}
+			else if (pos == size) {
+				DeleteLast();
+			}
+			else if (pos > 0 && pos < size) {
+				Node* current = head;
+				for (int i = 0; i < pos - 1; i++) {
+					current = current->Next;
+				}
+				Node* temp = current->Next;
+				current->Next = temp->Next;
+				delete temp;
+				size--;
+			}
+		}
 };
 
+
+		
